9e91793f5dc54ac18581468ba86f32a90a2cf446
Tom Young <tyoung@n.io>
2016-07-11 10:57:49 -0600

    Remove base block, correct whitespace errors

--- trend_analysis_block.py
+++ trend_analysis_block.py
@@ -1,11 +1,12 @@
-from .trend_analysis_base import TrendAnalysisBase
+from nio.block.base import Block
+from nio.signal.base import Signal
 from nio.util.discovery import discoverable
 from nio.properties import Property
 from nio.properties.version import VersionProperty
 
 
 @discoverable
-class TrendAnalysis(TrendAnalysisBase):
+class TrendAnalysis(Block):
 
     version = VersionProperty('0.1.0')
     x = Property(title='Data Set', default='')
@@ -17,3 +18,17 @@ class TrendAnalysis(TrendAnalysisBase):
             signal.trend=trend
             signal.trend_from=trend_from
         self.notify_signals(signals)
+    """
+    Perform least-squares-fit of linear regression to a list of numeric values
+    """
+    def linreg(self, X, Y):
+        N = len(X)
+        Sx = Sy = Sxx = Syy = Sxy = 0.0
+        for x, y in zip(X, Y):
+            Sx = Sx + x
+            Sy = Sy + y
+            Sxx = Sxx + x*x
+            Syy = Syy + y*y
+            Sxy = Sxy + x*y
+        det = Sxx * N - Sx * Sx
+        return (Sxy * N - Sy * Sx)/det, (Sxx * Sy - Sx * Sxy)/det
