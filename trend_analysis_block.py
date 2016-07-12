bf8b40e4f9463d9b13d0d1f344fbca6d7968ed29
Tom Young <tyoung@n.io>
2016-07-12 16:00:11 -0600

    require list type for input data set

--- trend_analysis_block.py
+++ trend_analysis_block.py
@@ -8,18 +8,21 @@ from nio.properties.version import VersionProperty
 @discoverable
 class TrendAnalysis(Block):
 
-    version = VersionProperty('0.1.0')
     x = Property(title='Data Set', default='')
+    version = VersionProperty('0.1.0')
     
     def process_signals(self, signals):
         for signal in signals:
-            x = self.x(signal)
-            trend,trend_start = self.linreg(range(len(x)),x)
-            trend_end = [trend * index + trend_start for index in range(len(x))][len(x)-1]
-            signal.trend = trend
-            signal.trend_start = trend_start
-            signal.trend_end = trend_end
-        self.notify_signals(signals)
+            if isinstance(self.x(signal), list):
+                x = self.x(signal)
+                trend,trend_start = self.linreg(range(len(x)),x)
+                trend_end = [trend * index + trend_start for index in range(len(x))][len(x)-1]
+                signal.trend = trend
+                signal.trend_start = trend_start
+                signal.trend_end = trend_end
+                self.notify_signals(signals)
+            else:
+                self.logger.exception("Data Set must be a list")
     """
     Perform least-squares-fit of linear regression to a list of numeric values
     """
