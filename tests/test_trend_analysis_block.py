e417952782787877d1b05cd223387822f69dfa1d
Tom Young <tyoung@n.io>
2016-07-14 14:43:40 -0600

    Update readme

--- tests/test_trend_analysis_block.py
+++ tests/test_trend_analysis_block.py
@@ -18,7 +18,6 @@ class TestExample(NIOBlockTestCase):
             Signal({"data": [1,2,3,4,5]}),
             Signal({"data": [3,3,3,3,3]})
             ])
-        # blk.process_signals([Signal({"data": [5,4,3,2,1]})])
         blk.stop()
         self.assert_num_signals_notified(3)
         self.assertDictEqual(
